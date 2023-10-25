8555daaea781567b7a2d0f2a57fa05626f0bc7cb
Danny Nguyen <dnguy206@ucsc.edu>
2023-10-25 03:53:09 -0700

    adding navbar

--- slugshop/src/components/NavBar.js
+++ slugshop/src/components/NavBar.js
@@ -1,13 +1,21 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
+
+
 function NavBar() {
-  return (
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click)
+    return (
     <>
         <nav className='navbar'>
             <div className='navbar-containter'>
                 <Link to="/" className="navbar-logo">
-                    SLUGSHOP
+                    SLUGSHOP <i className='i.fab.fa-typo3' />
                 </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
             </div>
         </nav>
     </>
