88a10c4078fd560f9d2b9b965ad5e3616ada035b
Michael Alaniz <michaelalaniz76@gmail.com>
2023-11-02 00:27:26 -0600

    config setup

---
+++ slugshop/src/pages/Login.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import App from '../App'
+import { directive } from '@babel/types';
+
+export default function Login(){
+    return(
+
+        <div className='bg-black h-96 w-full bg-cover bg-center p-24'>
+        </div>
+
+    )
+}
