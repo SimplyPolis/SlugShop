0ee8bb0090021bac7ddd55dd372daec861785b72
Michael Alaniz <michaelalaniz76@gmail.com>
2023-11-09 12:44:04 -0800

    New Additions to Page Styling

--- slugshop/src/App.js
+++ slugshop/src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <>
       <Router>
-        <NavBar />
         <Routes>
           <Route index element = {<Login/>}/>
         </Routes>
--- slugshop/src/pages/Login.js
+++ slugshop/src/pages/Login.js
@@ -7,26 +7,35 @@ export default function Login(){
     return(
 
         <div>
-            <div className="bg-yellow-300 w-full h-[300px]">
-                <form>
-                    <label className="bottom-96 left-96" for="fname">UCSC Email:</label>
-                    <br></br>
-                    <input type="text"> 
-                    
-                    </input>
-                    <br></br>
-                    <br></br>
-                    <label className="bottom-96 left-96" for="fname">Password:</label>
-                    <br></br>
-                    <input type="text">
+            <div className="bg-yellow-300 w-screen h-screen">
+                <div className="bg-yellow-300 w-minh-96 h-[250px]"></div>
+                    <div className=" pl-[750px]">
+                        <div className="bg-blue-600 rounded-xl w-96 h-96 flex items-center justify-center">
+                            <form>
+                                <label className="bottom-96 left-96" for="fname">UCSC Email:</label>
+                                <br></br>
+                                
+                                <input type="text"> 
+                                </input>
+                                
+                                <br></br>
+                                <br></br>
+                                
+                                <label className="bottom-96 left-96" for="fname">Password:</label>
+                                
+                                <br></br>
+                                
+                                <input type="text">
+                                </input>
 
-                    </input>
-                    <br></br>
-                    <br></br>
-                    
-                    <button className="bg-slate-600" onClick={""}>Login</button>
-
-                </form>    
+                                <br></br>
+                                <br></br>
+                                
+                                <button className="bg-white rounded w-12" onClick={""}>Login</button>
+                        
+                            </form>
+                        </div>    
+                    </div>
             </div>
         </div>
 
