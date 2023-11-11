b24b22bbe21ee4bb7882efc87b736ade25a01c40
Michael Alaniz <michaelalaniz76@gmail.com>
2023-11-10 16:00:38 -0800

    Updates to Styling

--- slugshop/src/pages/Login.js
+++ slugshop/src/pages/Login.js
@@ -7,35 +7,34 @@ export default function Login(){
     return(
 
         <div>
-            <div className="bg-yellow-300 w-screen h-screen">
-                <div className="bg-yellow-300 w-minh-96 h-[250px]"></div>
-                    <div className=" pl-[750px]">
-                        <div className="bg-blue-600 rounded-xl w-96 h-96 flex items-center justify-center">
-                            <form>
-                                <label className="bottom-96 left-96" for="fname">UCSC Email:</label>
-                                <br></br>
-                                
-                                <input type="text"> 
-                                </input>
-                                
-                                <br></br>
-                                <br></br>
-                                
-                                <label className="bottom-96 left-96" for="fname">Password:</label>
-                                
-                                <br></br>
-                                
-                                <input type="text">
-                                </input>
+            <div className="bg-[url('https://collegevine.imgix.net/571e9a11-964f-48a4-83d0-562584d134ac.jpg')] w-screen h-screen">          
+                <div className="pl-[750px] pt-[250px]">
+                    <div className="bg-blue-600 rounded-xl w-96 h-96 flex items-center justify-center">
+                        <form>
+                            <label className="pl-[-100px]" for="fname">UCSC Email:</label>
+                            <br></br>
+                            
+                            <input type="text"> 
+                            </input>
+                            
+                            <br></br>
+                            <br></br>
+                            
+                            <label className="bottom-96 left-96" for="fname">Password:</label>
+                            
+                            <br></br>
+                            
+                            <input type="text">
+                            </input>
 
-                                <br></br>
-                                <br></br>
-                                
-                                <button className="bg-white rounded w-12" onClick={""}>Login</button>
-                        
-                            </form>
-                        </div>    
-                    </div>
+                            <br></br>
+                            <br></br>
+                            
+                            <button className="bg-white rounded w-12" onClick={""}>Login</button>
+                    
+                        </form>
+                    </div>    
+                </div>
             </div>
         </div>
 
