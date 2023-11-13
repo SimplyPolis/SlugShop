4c0bcd948ca65f9c67715f21f10bb847a1e000d0
Michael Alaniz <michaelalaniz76@gmail.com>
2023-11-12 22:39:27 -0800

    Added User Listings Page

---
+++ slugshop/src/pages/UserListing.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import App from '../App'
+import { directive } from '@babel/types';
+
+
+export default function UserLogin(){
+
+    return(
+
+        <div>
+            
+        </div>
+
+    )
+}
