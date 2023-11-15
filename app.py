6e04ff87ee762dcb59557f89c2ad5314697164f9
Matthew Polis <mkp1557@gmail.com>
2023-11-15 13:58:10 -0800

    Updated to Python 3.12 and added file image submission to listings.

--- app.py
+++ app.py
@@ -1,42 +1,53 @@
 from async_oauthlib import OAuth2Session
 
-from quart import Quart, request, redirect, session, url_for, render_template_string,render_template
+from quart import Quart, request, redirect, session, url_for, render_template_string, render_template
 from quart.json import jsonify
 from quart_auth import AuthUser, current_user, login_required, login_user, logout_user, QuartAuth, Unauthorized
+from quart_uploads import UploadSet, configure_uploads, IMAGES, UploadNotAllowed
 import os
 import secrets
-listing_id=0
+import uuid
+import asyncio
+
 CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
 authorization_base_url = 'https://accounts.google.com/o/oauth2/auth'
 token_url = "https://accounts.google.com/o/oauth2/token"
-get_user_info ='https://www.googleapis.com/userinfo/v2/me?alt=json&access_token={}'
-app_url="https://localhost:5000"
+get_user_info = 'https://www.googleapis.com/userinfo/v2/me?alt=json&access_token={}'
+app_url = "https://localhost:5000"
+
+
 class Listing():
-    def __init__(self,title,text):
-        global listing_id
-        self.title=title
-        self.text=text
-        self.id=listing_id
-        listing_id+=1
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+        self.id = uuid.uuid4().hex
+
     @property
     def url(self):
         return f"{app_url}/listing/{self.id}"
-totsadb=dict()
-all_listing=[Listing("Listing 1","Listing 1 test"),Listing("Listing 2","Listing 2 test")]
+
+
+totsadb = dict()
+
+Listing1 = Listing("Listing 1", "Listing 1 test")
+Listing2 = Listing("Listing 2", "Listing 2 test")
+all_listing = {Listing1.id: Listing1, Listing2.id: Listing2}
+
+
 class User(AuthUser):
     def __init__(self, auth_id):
         super().__init__(auth_id)
         self._resolved = False
         self._email = None
-        self._name= None
+        self._name = None
 
     async def _resolve(self):
         if self._resolved:
             return
-        user_dict=totsadb.get(self.auth_id)
+        user_dict = totsadb.get(self.auth_id)
         self._email = user_dict["email"]
-        self._name=user_dict["name"]
+        self._name = user_dict["name"]
         self._resolved = True
 
     @property
@@ -50,65 +61,94 @@ class User(AuthUser):
         return self._name
 
 
-
 app = Quart(__name__)
 app.secret_key = secrets.token_urlsafe(16)
 auth_manager = QuartAuth(app)
 auth_manager.user_class = User
 # This information is obtained upon registration of a new GitHub
 
+app.config["UPLOADED_PHOTOS_DEST"] = '.\\listingpics'
+
+uploaded_photos = UploadSet('photos', IMAGES)
+
+configure_uploads(app, uploaded_photos)
+
 
 @app.route("/login")
 async def login():
-   async with OAuth2Session(CLIENT_ID,scope=["https://www.googleapis.com/auth/userinfo.profile","openid","https://www.googleapis.com/auth/userinfo.email"],redirect_uri=f"{app_url}/redirect") as google:
-        authorization_url, state = google.authorization_url(authorization_base_url, access_type="offline", prompt="select_account")
+    async with OAuth2Session(CLIENT_ID, scope=["https://www.googleapis.com/auth/userinfo.profile", "openid",
+                                               "https://www.googleapis.com/auth/userinfo.email"],
+                             redirect_uri=f"{app_url}/redirect") as google:
+        authorization_url, state = google.authorization_url(authorization_base_url, access_type="offline",
+                                                            prompt="select_account")
         # State is used to prevent CSRF, keep this for later.
         session['oauth_state'] = state
         return redirect(authorization_url)
 
 
 @app.route("/redirect")
-
 async def redirect_page():
-   async with OAuth2Session(CLIENT_ID, state=session['oauth_state'],scope=["https://www.googleapis.com/auth/userinfo.profile","openid","https://www.googleapis.com/auth/userinfo.email"],redirect_uri=f"{app_url}/redirect") as google:
+    async with OAuth2Session(CLIENT_ID, state=session['oauth_state'],
+                             scope=["https://www.googleapis.com/auth/userinfo.profile", "openid",
+                                    "https://www.googleapis.com/auth/userinfo.email"],
+                             redirect_uri=f"{app_url}/redirect") as google:
         token = await google.fetch_token(token_url, client_secret=CLIENT_SECRET, authorization_response=request.url)
-        request_url=get_user_info.format(token["access_token"])
-        request_reponse=await google.get(request_url)
+        request_url = get_user_info.format(token["access_token"])
+        request_reponse = await google.get(request_url)
         if request_reponse.ok:
-            request_reponse=await request_reponse.json()
+            request_reponse = await request_reponse.json()
             if not request_reponse["id"] in totsadb:
-                totsadb.update({request_reponse["id"]:{"email":request_reponse["email"],"name":request_reponse["name"]}})
+                totsadb.update(
+                    {request_reponse["id"]: {"email": request_reponse["email"], "name": request_reponse["name"]}})
             login_user(User(request_reponse["id"]))
         return redirect("/")
+
 
 @app.route("/")
 @login_required
 async def home():
     return await render_template("index.html")
 
+
 @app.route("/listings")
 @login_required
 async def listings():
-    return await render_template("listings.html",listings=all_listing)
+    return await render_template("listings.html", listings=all_listing.values())
+
 
 @app.route("/createlisting")
 @login_required
 async def createListingpage():
     return await render_template("createlisting.html")
 
-@app.route("/createlistingmethod",methods=["POST"])
+
+@app.route("/createlistingmethod", methods=["POST"])
 @login_required
 async def createlisting():
-    data = await request.get_data()
-    req_dict={entry.split(b'=')[0]:entry.split(b'=')[1 ]for entry in data.split(b'&')}
-    all_listing.append(Listing(req_dict[b"lname"],req_dict[b"ltext"]))
+    req_dict = await request.form
+    try:
+        files = (await asyncio.gather(request.files, return_exceptions=True))[0]
+    except:
+        print("ooops")
+    new_listing = Listing(req_dict["lname"], req_dict["ltext"])
+    all_listing.update({new_listing.id: new_listing})
+    try:
+        filename = await uploaded_photos.save(files.get("limage"), folder=new_listing.id)
+    except UploadNotAllowed:
+        print("ooops")
+
     return redirect("/listings")
 
+
 @app.route("/listing/<listing_id>")
+@login_required
 async def listinf(listing_id):
-    return await render_template("listing.html", listing=all_listing[int(listing_id)])
+    return await render_template("listing.html", listing=all_listing[listing_id])
+
+
 @app.errorhandler(Unauthorized)
 async def redirect_to_login(*_):
     return redirect(url_for("login"))
 
-app.run(host="localhost",port=5000,certfile=".\localhost.pem",keyfile=".\localhost-key.pem")
+
+app.run(host="localhost", port=5000, certfile=".\\localhost.pem", keyfile=".\\localhost-key.pem")
