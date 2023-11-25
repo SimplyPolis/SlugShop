ddbde4f93fdce78abaefe26bb48b5461419239e0
Michael Alaniz <michaelalaniz76@gmail.com>
2023-11-24 17:42:01 -0800

    Inidiv Listing Demo

--- slugshop/src/App.js
+++ slugshop/src/App.js
@@ -2,16 +2,32 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import Login from './pages/Login';
+import UserListing from './pages/UserListing';
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material';
 
 function App() {
+
+  const theme= createTheme({
+      typography: {
+          fontFamily:[
+            'Josefin Sans','sans-serif'
+          ].join(','),
+      }
+    });
+
+
   return (
     <>
+
+      <ThemeProvider theme={theme}>
       <Router>
         <Routes>
           <Route index element = {<Login/>}/>
+          <Route path="/user" element ={<UserListing/>}/>
         </Routes>
       </Router>
+      </ThemeProvider>
     </>
   );
 }
--- slugshop/src/pages/UserListing.js
+++ slugshop/src/pages/UserListing.js
@@ -1,16 +1,56 @@
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import App from '../App'
+import NavBarOther from '../components/NavBarOther';
 import { directive } from '@babel/types';
+import { AppBar, Avatar, Typography, createTheme } from '@mui/material';
+import {Toolbar} from '@mui/material';
+import { Container } from '@mui/material';
+import { Grid } from '@mui/material';
+import { Paper } from '@mui/material';
 
 
-export default function UserLogin(){
-
+export default function UserListing(){
     return(
+        <>
+            <NavBarOther/>  
+            <Grid container spacing={1} rowSpacing={1}>
+                <Grid item xs={5} spacing={12} className="pt-32">
+                    
+                        <img 
+                        className = "pt-[8rem] pl-24 w-[40rem]" 
+                        src="https://i.ebayimg.com/images/g/i8gAAOSwSatiDUg1/s-l1600.jpg" 
+                        alt="Placeholder"/>
+                    
+                </Grid>  
+                <Grid item className=" pt-72" xs={5}> 
+                    
+                    <Typography fontSize="30px" spacing={30}className="pt-[9rem]">
+                        Vintage Sweater
+                    </Typography>
+                    <Typography className=" pr-44">
+                        $15.99
+                    </Typography>
+                    <Typography className="pr-44">
+                        Size: L
+                    </Typography>
+                    <Typography className="pr-[7rem]">
+                        Condition: New
+                    </Typography>
+                    
+                </Grid>
+                <Grid item xs={5}>
+                    
+                    <Typography className=" pl-[6rem]">
+                    <button className="bg-blue-900 text-slate-50 rounded w-[21rem] h-14" onClick={""}>Contact Seller</button>
+                    </Typography>
+                    
+                </Grid>
+            </Grid>
 
-        <div>
-            
-        </div>
+
+
+        </>
 
     )
 }
