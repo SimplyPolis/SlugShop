1a5cb26e12395e39d11022975e330182dd5e6910
Michael Alaniz <michaelalaniz76@gmail.com>
2023-11-27 14:07:17 -0800

    Additions to WebPages

--- slugshop/src/pages/CreateListing.js
+++ slugshop/src/pages/CreateListing.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import App from '../App'
 import NavBarOther from '../components/NavBarOther';
 import { directive } from '@babel/types';
@@ -14,22 +14,43 @@ import { IconButton } from '@mui/material';
 
 export default function CreateListing(){
 
+    const [listing, setListing]= useState({
+
+        item_name: "",
+        price: "",
+        size: "",
+        condition: ""
+    })
+
+
     return(
 
         <>
             <NavBarOther/>
-            <Grid container className="pt-[8rem] pl-16" spacing={5}>
-                <Grid item>
-                    <Typography fontSize="30px">Item Name:</Typography>
-                    <TextField id="standard-basic" variant="standard"/>
-                    <Typography className="pr-[5.5rem]" fontSize="30px">Price:</Typography>
-                    <TextField id="standard-basic" variant="standard"/>
-                    <Typography className="pr-[6.4rem]" fontSize="30px">Size:</Typography>
-                    <TextField id="standard-basic" variant="standard"/>
-                    <Typography className="pr-[2rem]" fontSize="30px">Condition:</Typography>
-                    <TextField id="standard-basic" variant="standard"/>
+            <Grid container className="pt-[8rem] pl-16" spacing={5} columnSpacing={5}>
+                <Grid item xs={20}>
+                    
+                        <Typography fontSize="30px">Item Name:</Typography>
+                        <TextField id="standard-basic" variant="standard"/>
+
+                        <Typography className="pr-[5.5rem]" fontSize="30px">Price:</Typography>
+                        <TextField id="standard-basic" variant="standard"/>
+
+                        <Typography className="pr-[6.4rem]" fontSize="30px">Size:</Typography>
+                        <TextField id="standard-basic" variant="standard"/>
+
+                        <Typography className="pr-[2rem]" fontSize="30px">Condition:</Typography>
+                        <TextField id="standard-basic" variant="standard"/>
                     
                 </Grid>
+            <Grid item xs={20}>
+                <Typography>
+                    <button className="bg-yellow-500 rounded w-20 text-white">
+                        Post
+                    </button>
+
+                </Typography>
+            </Grid>
             </Grid>
 
         </>
